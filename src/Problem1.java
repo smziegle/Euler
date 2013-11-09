2da3790a7fa05899bdbbc304b2f57e43b31ad064
smziegle <smziegle@g.cofc.edu>
2013-11-09 13:47:07 -0500

    First Commit

---
+++ src/Problem1.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Scott Ziegler
+ * Date: 11/9/13
+ * Time: 1:40 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Problem1 {
+}
